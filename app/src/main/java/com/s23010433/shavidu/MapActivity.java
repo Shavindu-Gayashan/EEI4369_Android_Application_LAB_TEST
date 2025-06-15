78dc264374d4aed06bbe7f7a59bd3abf8c6af5fd
Shavindu-Gayashan <shavindugaya@gmail.com>
2025-06-15 20:14:35 +0530

    Task 3 Completed

--- app/src/main/java/com/s23010433/shavidu/MapActivity.java
+++ app/src/main/java/com/s23010433/shavidu/MapActivity.java
@@ -1,6 +1,13 @@
 package com.s23010433.shavidu;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,28 +18,55 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+import java.util.Locale;
+
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private GoogleMap myMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
+
+        EditText addressInput = findViewById(R.id.address_input);
+        Button searchButton = findViewById(R.id.search_button);
+
+        searchButton.setOnClickListener(v -> {
+            String address = addressInput.getText().toString();
+            if (address.isEmpty()) {
+                Toast.makeText(this, "Please enter an address", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> addresses = geocoder.getFromLocationName(address, 1);
+                if (addresses != null && !addresses.isEmpty()) {
+                    Address location = addresses.get(0);
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    if (myMap != null) {
+                        myMap.clear();
+                        myMap.addMarker(new MarkerOptions().position(latLng).title(address));
+                        myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15.0f));
+                    }
+                } else {
+                    Toast.makeText(this, "Location not found", Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Error finding location", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        mMap = googleMap;
-        LatLng openuniversity = new LatLng(6.882945849884587, 79.88669183624769);
-        mMap.addMarker(new MarkerOptions().position(openuniversity).title("Open University"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(openuniversity, 18.0f));
-        mMap.getUiSettings().setZoomControlsEnabled(true);
+        myMap = googleMap;
     }
 }
